259c19fae6606a9cd899ee62cc03b07ba6f4a75b
ruan-silva1 <ruan.jdsilva@senacsp.edu.br>
2025-03-09 13:32:16 -0300

    adicionando algoritmo planetas e frutas em kotlin

--- projeto-4-kotlin/src/main/kotlin/Main.kt
+++ projeto-4-kotlin/src/main/kotlin/Main.kt
@@ -3,7 +3,12 @@ package org.example
 var qtdeEstudantes = 0
 var estudantes = mutableListOf<String>()
 
-fun main() {
+fun main(){
+    //verificarPlanetas()
+    verificarFrutas()
+    //fazerCadastroEstudante()
+}
+fun fazerCadastroEstudante() {
     cadastrarEstudante()
     println("Deseja cadastrar mais estudantes? (s/n)")
     var resposta = readln()
@@ -21,4 +26,37 @@ fun cadastrarEstudante() {
     val nome = readln()
     estudantes.add(nome)
     qtdeEstudantes++
+}
+
+fun verificarPlanetas(){
+    val planetas = setOf<String>("Terra", "Marte", "Plutão", "Vênus", "Júpiter", "Saturno")
+    println("digite um planeta")
+    val planeta = readln()
+    if(planetas.contains(planeta)){
+        println("planeta já está incluido")
+    }
+    else{
+        println("planeta nao esta incluido")
+    }
+}
+
+fun verificarFrutas(){
+    val frutas = mutableListOf("maçã", "banana", "laranja", "uva", "morango", "abacaxi", "manga", "kiwi", "melancia", "pêssego")
+    println("digite o nome de uma fruta!")
+    val fruta = readln()
+
+    if (frutas.contains(fruta)) {
+        val iterator = frutas.iterator()
+        while (iterator.hasNext()) {
+            val e = iterator.next()
+            if (fruta == e) {
+                iterator.remove() // Remove o elemento de forma segura
+                println("Fruta retirada da lista: $e")
+            }
+        }
+    } else {
+        println("Fruta não encontrada na lista.")
+    }
+
+    println("Lista atualizada: $frutas")
 }
